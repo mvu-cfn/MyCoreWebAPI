771df530d2c43a36147ca361a80e8eebb33a00aa
Minh Vu <mvu@commonwealth.com>
2020-05-10 21:47:25 -0400

    some clean up

--- MyCoreWebAPI/WeatherForecast.cs
+++ MyCoreWebAPI/WeatherForecast.cs
@@ -4,12 +4,14 @@ namespace MyCoreWebAPI
 {
     public class WeatherForecast
     {
+        //weatherforcast model
         public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
+        public int TemperatureCelcius { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureFahrenheit => 32 + (int)(TemperatureCelcius / 0.5556);
 
         public string Summary { get; set; }
+
     }
 }
